873b26c5120914bdd297abb08183b655bfc054cd
adwitafernanda <adwitafernanda4@gmail.com>
2023-05-25 11:07:23 +0700

    menambahkan kondisi untuk current

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Node {
 public:
 	int noMhs;
-	string nama;
+	string name;
 	Node* next;
 	Node* prev;
 };
@@ -31,5 +31,42 @@ DoubleLinkedList::DoubleLinkedList() {
 }
 
 void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin.ignore();
+	getline(cin, nm);
+	Node* newNode = new Node();
+	newNode->noMhs = nim;
+	newNode->name = nm;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
+
+	Node* current = START;
+		while (current->next != NULL && nim == current->next->noMhs)
+			current = current->next;
+
+		if (current->next != NULL && nim == current->next->noMhs) {
+			cout << "|nDuplicate roll numbers not allowed" << endl;
+			return;
+		}
+
+		newNode->next = current->next;
+		newNode->prev = current;
+		if (current->next != NULL)
+			current->next->prev = newNode;
+		current->next = newNode;
 }
