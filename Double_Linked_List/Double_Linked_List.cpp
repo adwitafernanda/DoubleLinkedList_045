00be488a795acf3b95f769e7a3bb640ae3308e0f
adwitafernanda <adwitafernanda4@gmail.com>
2023-05-25 11:48:57 +0700

    selesai membuat doublelinkedlist hapus

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -134,4 +134,10 @@ void DoubleLinkedList::hapus() {
 	if (listEmpty()) {
 		cout << "\nlist is empty" << endl;
 	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted:  ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "record not found" << endl;
 }
